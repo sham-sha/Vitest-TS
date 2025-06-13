dffbe8680ccd585ea4f0342e9d7ea2572251867e
sham-sha <sham08092003@gmail.com>
2025-06-13 19:14:30 +0530

    changes

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -15,8 +15,23 @@ describe('TermsAndConditions', () => {
          const button = screen.getAllByRole('button')
          expect(button).toBeInTheDocument;
          expect(button).toBeDisabled
+
     });
 
+        it('should render with correct text and initial state  ', () => {
+        render(<TermsAndConditions/>)
+         const heading = screen.getByRole('heading');
+         expect(heading).toBeInTheDocument()
+         expect(heading).toHaveTextContent('Terms')
+
+         const checkbox = screen.getByRole('checkbox')
+         expect(checkbox).not.toBeChecked()        
+         
+         const button = screen.getAllByRole('button')
+         expect(button).toBeInTheDocument;
+         expect(button).toBeDisabled
+
+    });
       
 
 
